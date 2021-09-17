d92459577f4aaf3ac978a64188eb13ab91444275
TrueReflect-Lead-Design <90807740+TrueReflect-Lead-Design@users.noreply.github.com>
2021-09-17 19:20:08 -0400

    Update MasterChef.sol

--- MasterChef.sol
+++ MasterChef.sol
@@ -4,6 +4,48 @@
 
 pragma solidity 0.8.7;
 
+abstract contract ReentrancyGuard {
+    // Booleans are more expensive than uint256 or any type that takes up a full
+    // word because each write operation emits an extra SLOAD to first read the
+    // slot's contents, replace the bits taken up by the boolean, and then write
+    // back. This is the compiler's defense against contract upgrades and
+    // pointer aliasing, and it cannot be disabled.
+
+    // The values being non-zero value makes deployment a bit more expensive,
+    // but in exchange the refund on every call to nonReentrant will be lower in
+    // amount. Since refunds are capped to a percentage of the total
+    // transaction's gas, it is best to keep them low in cases like this one, to
+    // increase the likelihood of the full refund coming into effect.
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+
+    uint256 private _status;
+
+    constructor () {
+        _status = _NOT_ENTERED;
+    }
+
+    /**
+     * @dev Prevents a contract from calling itself, directly or indirectly.
+     * Calling a `nonReentrant` function from another `nonReentrant`
+     * function is not supported. It is possible to prevent this from happening
+     * by making the `nonReentrant` function external, and make it call a
+     * `private` function that does the actual work.
+     */
+    modifier nonReentrant() {
+        // On the first call to nonReentrant, _notEntered will be true
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+
+        // Any calls to nonReentrant after this point will fail
+        _status = _ENTERED;
+
+        _;
+
+        // By storing the original value once again, a refund is triggered (see
+        // https://eips.ethereum.org/EIPS/eip-2200)
+        _status = _NOT_ENTERED;
+    }
+}
 abstract contract Ownable {
     address internal _owner;
     address internal _dev;
@@ -406,7 +448,7 @@ contract TrueReflect is IERC20, Ownable {
 }
 
 
-contract MasterChef is Ownable {
+contract MasterChef is Ownable, ReentrancyGuard {
     using SafeMath for uint256;
 
     // Info of each user.
@@ -567,6 +609,7 @@ contract MasterChef is Ownable {
         if (readUserLock(_pid, msg.sender)) {
             if (pending > 0 || user.rewardLockup > 0) {
                 uint256 totalRewards = pending.add(user.rewardLockup);
+                
                 user.rewardDebt = user.amount.mul(pool.accTokenPerShare).div(BPS);
                 user.rewardLockup = 0;
                 user.unlockupTime = block.timestamp.add(1 minutes);
@@ -579,7 +622,7 @@ contract MasterChef is Ownable {
     }
 
     // Deposit LP tokens to MasterChef for TOKEN allocation.
-    function deposit(uint256 _pid, uint256 _amount) public {
+    function deposit(uint256 _pid, uint256 _amount) public nonReentrant{
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
         updatePool(_pid);
@@ -602,36 +645,33 @@ contract MasterChef is Ownable {
         user.rewardDebt = user.amount.mul(pool.accTokenPerShare).div(BPS);
         emit Deposit(msg.sender, _pid, _amount);
     }
-
+    
     // Withdraw LP tokens from MasterChef.
-    function withdraw(uint256 _pid, uint256 _amount) public {
+    function withdraw(uint256 _pid, uint256 _amount) public nonReentrant {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
         require(user.amount >= _amount, "withdraw: not good");
         updatePool(_pid);
-        processHarvest(_pid);
-         
+        uint256 pending = user.amount.mul(pool.accTokenPerShare).div(1e12).sub(user.rewardDebt);
+        if(pending > 0) {
+            safeTokenTransfer(msg.sender, pending);
+        }
         if(_amount > 0) {
             user.amount = user.amount.sub(_amount);
             pool.lpToken.transfer(address(msg.sender), _amount);
-            
         }
-
-        user.rewardDebt = user.amount.mul(pool.accTokenPerShare).div(BPS);        
+        user.rewardDebt = user.amount.mul(pool.accTokenPerShare).div(1e12);
         emit Withdraw(msg.sender, _pid, _amount);
     }
 
     // Withdraw without caring about rewards. EMERGENCY ONLY.
-    function emergencyWithdraw(uint256 _pid) public {
+    function emergencyWithdraw(uint256 _pid) public nonReentrant{
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
         uint256 amount = user.amount;
         user.amount = 0;
         user.rewardDebt = 0;
-
         pool.lpToken.transfer(address(msg.sender), amount);
-
-    
         emit EmergencyWithdraw(msg.sender, _pid, amount);
     }
 
@@ -640,7 +680,6 @@ contract MasterChef is Ownable {
         uint256 tokenBal = token.balanceOf(address(this));
         if (_amount > tokenBal) {
             token.transfer(_to, tokenBal);
-            
         } else {
             token.transfer(_to, _amount);
         }
@@ -649,7 +688,6 @@ contract MasterChef is Ownable {
     //Pancake has to add hidden dummy pools inorder to alter the emission, here we make it simple and transparent to all.
     function updateEmissionRate(uint256 _reflectPerSecond) external onlyDev {
         massUpdatePools();
-        
         reflectPerSecond = _reflectPerSecond;
     }
     
